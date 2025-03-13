39153d35f00f1e759e60f9279cc356e6d67b0b35
rod <dev_rodts@outlook.com>
2025-03-13 09:04:37 -0300

    feat: add ProductJpaRepository

--- src/main/java/com/devrodts/products/products/infra/persistence/ProductJpaRepository.java
+++ src/main/java/com/devrodts/products/products/infra/persistence/ProductJpaRepository.java
@@ -1,4 +1,32 @@
 package com.devrodts.products.products.infra.persistence;
+import com.devrodts.products.products.domain.models.ProductModel;
+import com.devrodts.products.products.infra.repository.ProductRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public class ProductJpaRepository {
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ProductJpaRepository extends JpaRepository<ProductModel, Long>, ProductRepository {
+
+    @Override
+    default Optional<ProductModel> findById(Long id) {
+        return findById(id);
+    }
+
+    @Override
+    default List<ProductModel> findAll() {
+        return findAll();
+    }
+
+    @Override
+    default ProductModel save(ProductModel product) {
+        return save(product);
+    }
+
+    @Override
+    default void deleteById(Long id) {
+        deleteById(id);
+    }
 }
