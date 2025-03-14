8c1b1282efd73b01b8be42a5f60770ba965f0b28
rod <dev_rodts@outlook.com>
2025-03-14 15:41:42 -0300

    feat: add ProductsCotroller

--- src/main/java/com/devrodts/products/products/presentation/controllers/ProductsController.java
+++ src/main/java/com/devrodts/products/products/presentation/controllers/ProductsController.java
@@ -1,4 +1,34 @@
 package com.devrodts.products.products.presentation.controllers;
 
+import com.devrodts.products.products.application.usecases.ProductCreateUseCase;
+import com.devrodts.products.products.presentation.dtos.CreateProductDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("products")
+@RequiredArgsConstructor
 public class ProductsController {
+
+    private final ProductCreateUseCase createProductUseCase;
+
+    @PostMapping
+    public ResponseEntity<Void> createProduct(@RequestBody CreateProductDTO createProductDTO) {
+        try {
+            createProductUseCase.execute(createProductDTO);
+            return ResponseEntity.ok().build();
+        } catch (Exception e) {
+            throw new RuntimeException("Error creating product", e);
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<Void> getProducts() {
+        try {
+            return ResponseEntity.ok().build();
+        }catch (Exception e) {
+                throw new RuntimeException("Error getting products", e);
+        }
+    }
 }
