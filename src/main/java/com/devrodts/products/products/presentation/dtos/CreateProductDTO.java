08cd8a83fc88b5176e4b8e313198a854d048b3c8
rod <dev_rodts@outlook.com>
2025-03-14 15:42:10 -0300

    create user DTO

--- src/main/java/com/devrodts/products/products/presentation/dtos/CreateProductDTO.java
+++ src/main/java/com/devrodts/products/products/presentation/dtos/CreateProductDTO.java
@@ -1,16 +1,21 @@
 package com.devrodts.products.products.presentation.dtos;
 
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.Min;
+import jakarta.persistence.GenerationType;
 import jakarta.validation.constraints.NotBlank;
 import java.math.BigDecimal;
+import java.util.UUID;
+
 
 
 public record CreateProductDTO(
 
         @NotBlank(message = "The id should be filled.")
-        @Id()
-        String id,
+        @Id
+        @GeneratedValue(strategy = GenerationType.UUID)
+        UUID productId,
 
         @NotBlank(message = "The name should be filled.")
         String name,
@@ -18,12 +23,7 @@ public record CreateProductDTO(
         @NotBlank(message = "The price should be filled")
         @Min(value = 0, message = "The price should be greater than or equal to zero.")
         BigDecimal price,
-
-        @NotBlank(message = "The product needs to have a description")
-        String description,
-
-        @NotBlank(message = "The stock cant be empty.")
-        Integer stock,
+        @NotBlank(message = "The stock cant be empty.") int stock,
 
         @NotBlank(message = "The image cant be empty.")
         String image,
